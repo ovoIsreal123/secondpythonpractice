0ec0a130587708eb3492b38774bfe86c6750fbf8
ovoIsreal23 <isrealgheneovo@gmail.com>
2025-08-24 17:09:28 -0700

    seconone

---
+++ Documents/secondpythonpractice/seconone.py
@@ -0,0 +1,6 @@
+num1 = float(input("any number between o-9"))
+num2 = float(input (" anyother number between 0-9"))
+operator = input(" place any arithmetic operator here")
+if operator == "+":
+    result = num1 + num2
+    print(result)
